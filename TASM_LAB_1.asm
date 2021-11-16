82824c0bcdb5521d920b43877187e425f2832f33
Vestriger <gerashchenko.p@mail.ru>
2021-11-16 18:58:24 +0300

    First lab

--- TASM_LAB_1.asm
+++ TASM_LAB_1.asm
@@ -18,7 +18,6 @@ call readNum
 mov c, bx
 call readNum
 mov d, bx
-; check:  if (a > b)
 mov ax, a
 mul c
 mov bx, b
@@ -30,8 +29,7 @@ mov bx, d
 cmp ax, bx
 jg printFirst
 
-; if all first condition is false:
-fistAllConditionIsFalse:
+fistFalse:
 mov ax, d
 add ax, a
 mov bx, b
@@ -62,15 +60,13 @@ jmp print
 printSecond:
 mov ax, a
 cmp ax, b
-jg fistAllConditionIsFalse
+jg fistFalse
 mov ax, a
 mul a
 sub ax, b
 add ax, c
 jmp print
 
-
-; read and print nums
 print:
 call printNum
 
@@ -80,50 +76,36 @@ mov al, 0
 int 21h
 
 readNum:
-; тк bx используется, то обнуляем
 mov bx, 0
-; получение числа
 mov ah, 01h
 int 21h
-; сравнение со знаком минуса
 cmp al, 2dh
 je negativeNum
-; если либо цифра больше 0, либо конец строки
 call analyze
 ret
 
 negativeNum:
-; меняем знак с положительного на отрицательный у введенного числа
 call readPositiveNum
 not bx 
 inc bx
 ret
 
 readPositiveNum:
-; чтение положительного числа
 mov ah, 01h
 int 21h
 
 analyze:
-; проверки на \r и \n
 cmp al, 0dh
 je endl
 cmp al, 10
 je endl
-; получение числа
 sub al, 48
-; обнуление ah чтобы там ниче не было
 mov ah, 0
 push ax
-; для получения числа (т.к. по символам принимаем) 
-; то нужно умножать
-; на 10 каждый раз, ибо 10тичная система
 mov ax, 10
 mul bx
 mov bx, ax
 pop ax
-; в bx хранится конечное число, к которому 
-; мы рекурсией прибавляем ax (новый введенный символ)
 add bx, ax
 call readPositiveNum
 
@@ -145,14 +127,12 @@ inc ax
 printPositiveNum:
 cmp ax, 0
 jz zero
-; перевод обратно в аски
 mov dx, 0
 mov bx, 10
 div bx    
 add dl, 48
 push dx
 call printPositiveNum
-; вывод в консоль dx (по символу рекурсией)
 pop dx
 mov ah, 02h
 int 21h
@@ -161,11 +141,8 @@ zero:
 ret
 
 printZero:
-; выводим ноль ('0')
 mov dl, 30h
-; говорим о том, что хотим вывести
 mov ah, 02h
-; dos прирывание
 int 21h
 ret
 
